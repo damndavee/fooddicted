3660283c36b11ba08b20d7cacd7bc04e250d7c9b
Pawel Kopij <pawel.kopij@capgemini.com>
2023-10-02 15:42:56 +0200

    change Hero section to accept children prop.

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,36 +1,19 @@
-import { StyleSheet, Text, View, ImageBackground,  } from 'react-native'
-import React, { useState } from 'react'
-import IconButton from '../buttons/IconButton'
-import { COLORS } from '../../utils/tokens'
-import SearchBar from '../form/SearchBar'
-import { Button } from 'native-base'
+import React, { ReactElement } from 'react'
+import { StyleSheet, View, ImageBackground,  } from 'react-native'
 
 type HeroProps = {
-    isHistoryOpen: boolean;
-    onSearch: () => void;
-    onGoToProfile: () => void;
+    children: ReactElement;
 }
 
 const Hero = (props: HeroProps) => {
   return (
     <View style={styles.heroContainer}>
         <ImageBackground source={require('../../../assets/hero-img.jpg')} style={styles.imgBackground} resizeMode='cover'>
-            <View style={styles.contentContainer}>
-                <View style={styles.profile}>
-                    <IconButton showBackgroundColor type='Secondary' isRounded onPress={props.onGoToProfile} name='people-outline' size={24} />
-                </View>
-                <View>
-                    <Text style={styles.heading}>Tell us, |userName|</Text>
-                    <Text style={styles.heading}>what would You like to cook?</Text>
-                </View>
-                <View style={styles.searchBarContainer}>
-                    <SearchBar text='Search...' width='200' showIcons isOpen={props.isHistoryOpen} onSearchPress={props.onSearch} />
-                </View>
-            </View>
+                {props.children}
         </ImageBackground>
     </View>
   )
-}
+};
 
 export default Hero
 
@@ -39,35 +22,9 @@ const styles = StyleSheet.create({
         height: '50%',
         zIndex: 1000,
     },
-    contentContainer: {
-        position: 'relative',
-        height: '100%',
-        width: '100%',
-        padding: 10,
-        justifyContent: 'flex-end',
-        gap: 10
-    },
-    searchBarContainer: {
-        flexDirection: 'row',
-        gap: 5,
-    },
     imgBackground: {
         width: '100%',
         height: '100%',
         flex: 1,
-    },
-    profile: {
-        position: 'absolute',
-        top: 10,
-        right: 10,
-    },
-    heading: {
-        color: COLORS.navbar_light,
-        fontSize: 20,
-        alignSelf: 'flex-start',
-        fontWeight: 'bold',
-        textShadowColor: '#000',
-        textShadowOffset: {height: 2, width: 2},
-        textShadowRadius: 2
     },
 })
