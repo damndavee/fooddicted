92479f91d25161193182fa66d5310096636fb5dd
Pawel Kopij <pawel.kopij@capgemini.com>
2023-11-26 19:29:50 +0100

    add image async loader, add new images to replace background

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import 'react-native-reanimated';
 
+import { View, Text } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { NativeBaseProvider } from 'native-base';
@@ -11,8 +12,24 @@ import { PersistGate } from 'redux-persist/integration/react';
 import { Navigation } from './src/navigation';
 
 import { persistor, store } from './src/store/store';
+import useImages from './src/hooks/useImages';
 
 export default function App() {
+
+  const [imagesLoaded] = useImages([
+    require('./assets/signinScreen.jpg'),
+    require('./assets/signupScreen.jpg'),
+    require('./assets/welcomeScreen.jpg'),
+    require('./assets/all-recipes.jpg'),
+    require('./assets/dashboard.jpg'),
+    require('./assets/hero-img.jpg'),
+  ])
+
+  if(!imagesLoaded) {
+    // Loading Spinner
+    return <View><Text>Loading</Text></View>
+  }
+
   return (
     <SafeAreaProvider>
       <GestureHandlerRootView style={{ flex: 1 }}>
